336d06fd79ef9090851c0044f608d9e1491765e8
Filloax <filloax@gmail.com>
2022-05-11 18:47:41 +0200

    bitset128

---
+++ meta/BitSet128.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@class BitSet128
+---@field l integer
+---@field h integer
+
+---@param l? integer @default = 0
+---@param h? integer @default = 0
+---@return BitSet128
+function _G.BitSet128(l, h)
+end
